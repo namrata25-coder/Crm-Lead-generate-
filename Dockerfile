2db696aa323994f1dfbfb0a28709ea68693ff723
Namrata pawar <namrata25-coder>
2026-09-23 07:37:32 +0530

    Update EF Core migration

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,7 @@ RUN dotnet publish "CrmLeadManagement.csproj" \
 FROM mcr.microsoft.com/dotnet/aspnet:10.0 AS final
 
 WORKDIR /app
-
+ 
 COPY --from=build /app/publish .
 
 ENV ASPNETCORE_URLS=http://+:10000
--- obj/Debug/net10.0/CrmLeadManagement.AssemblyInfo.cs
+++ obj/Debug/net10.0/CrmLeadManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrmLeadManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c4beb787d7cbdfb95ae296a717b86d6a970642")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3fbe24624910ecd9dd84f350532ba6ec6f22fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrmLeadManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrmLeadManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
